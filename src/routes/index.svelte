822b7ea16cea3400e0c92fa4912d4c5140ec8187
Kittipos Wajakajornrit <boss.zz@me.com>
2022-08-17 11:16:54 +0700

    Reduce header size

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,9 +1,10 @@
 <script lang="ts">
   import { onMount, onDestroy } from 'svelte'
+  import Wheel from '$lib/components/Wheel.svelte'
 
   let head = ['S', 'P', 'I', 'N', 'N', 'E', 'R']
   let titleInterval = null
-  const headClassBase = 'w-12 h-12 sm:w-20 sm:h-20 px-4 py-2 rounded-xl sm:rounded-2xl text-center'
+  const headClassBase = 'w-11 h-11 sm:w-14 sm:h-14 px-4 py-2 rounded-xl sm:rounded-2xl text-center'
   const headClassMod = headClassBase + ' bg-white dark:bg-black text-teal-800 dark:text-teal-200'
   let headClass = [headClassMod, headClassBase]
 
@@ -36,10 +37,11 @@
 
 <div class="w-full h-screen flex flex-col justify-center items-center bg-white dark:bg-black">
   <h1
-    class="text-2xl sm:text-6xl text-white bg-teal-800 dark:text-black dark:bg-teal-200 p-2 sm:p-4 rounded-2xl font-bold select-none flex flex-row gap-1 sm:gap-3 lg:gap-8"
+    class="text-xl sm:text-4xl text-white bg-teal-800 dark:text-black dark:bg-teal-200 p-2 sm:p-4 rounded-2xl font-bold select-none flex flex-row gap-1 sm:gap-2 lg:gap-5"
   >
     {#each head as h, i}
       <span class={headClass[i % 2]}>{h}</span>
     {/each}
   </h1>
+  <Wheel />
 </div>
